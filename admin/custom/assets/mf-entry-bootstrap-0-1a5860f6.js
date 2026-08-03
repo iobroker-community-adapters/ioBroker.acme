425951d3655a5f17e497b0d72a516060172ba206
GermanBluefox <dogafox@gmail.com>
2026-08-03 22:34:04 +0300

    chore: release v4.0.3
    - (@GermanBluefox) Migrated to admin 8

--- admin/custom/assets/mf-entry-bootstrap-0-1a5860f6.js
+++ admin/custom/assets/mf-entry-bootstrap-0-1a5860f6.js
@@ -26,11 +26,11 @@ const __mfImport = (src) =>
 
 
 (async () => {
-  const __mfHostInit = await __mfImport("./hostInit-BIvYm_2d.js");
+  const __mfHostInit = await __mfImport("./hostInit-DEYlsqYX.js");
   await __mfHostInit.__tla;
   const { initHost } = __mfHostInit;
   await initHost();
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-DRr0AMZn.js"));
+})().then(() => __mfImport("./index-ZK8sG-dx.js"));
